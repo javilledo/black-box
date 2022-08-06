90e1b991dd2ed53e356cda6c0a369ba5d9c2c4ee
Francisco Javier Lledó Fuster <javilledo@gmail.com>
2022-08-06 19:31:44 +0200

    solucionblackbox1 solved without for!

--- solucionblackbox1.py
+++ solucionblackbox1.py
@@ -2,6 +2,11 @@
 def filtro_por_anyo(secuencia: list[str], anyo: int) -> list[str]:
     return [el for el in secuencia if el[0:4] == str(anyo)]
 
+def filtro_por_anyo(secuencia: list[str], anyo: int) -> list[str]:
+    result = list(map(lambda el: el if (el[0:4] == str(anyo)) else False, secuencia))
+    result = list(filter(None, result))
+    return result
+
 res = filtro_por_anyo(["2020-1-4", "2019-4-7", "2021-12-11", "2020-8-9"], 2020)
 # ["2020-1-4", "2020-8-9"]
 
