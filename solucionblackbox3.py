f0025f045c8459087121a032b49be8691c589c14
Francisco Javier Lledó Fuster <javilledo@gmail.com>
2022-08-27 08:11:11 +0200

    char_hide implementado

--- solucionblackbox3.py
+++ solucionblackbox3.py
@@ -2,7 +2,7 @@
 def ocultar_numeros(numeros: str|int, char_hide: str = "x") -> str:
     numeros = str(numeros).replace(" ", "")
     if len(numeros) != 16: return False
-    return 'xxxxxxxxxxxxx' + numeros[-3:]
+    return char_hide*13 + numeros[-3:]
 
 #Tests
 assert ocultar_numeros(1234567890123456) == 'xxxxxxxxxxxxx456'
@@ -11,3 +11,4 @@ assert ocultar_numeros("1234 5678 9012 3456") == 'xxxxxxxxxxxxx456'
 assert ocultar_numeros(12345678901234567) == False
 assert ocultar_numeros("12345678901234567") == False
 assert ocultar_numeros("1234 5678 9012 3456 7") == False
+assert ocultar_numeros(1234567890123456, '-') == '-------------456'
